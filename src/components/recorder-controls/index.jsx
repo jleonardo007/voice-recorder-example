6e3289a39a831a2aa3c0386211be661c3aa7f389
leonardo bravo <j.leonardo.bravo@gmail.com>
2021-08-16 12:31:38 -0500

    Add recorder components

---
+++ src/components/recorder-controls/index.jsx
@@ -0,0 +1,45 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMicrophone, faTimes, faSave } from "@fortawesome/free-solid-svg-icons";
+import { formatMinutes, formatSeconds } from "../../utils/format-time";
+import "./styles.css";
+
+export default function RecorderControls({ recorderState, handlers }) {
+  const { recordingMinutes, recordingSeconds, initRecording } = recorderState;
+  const { startRecording, saveRecording, cancelRecording } = handlers;
+
+  return (
+    <div className="controls-container">
+      <div className="recorder-display">
+        <div className="recording-time">
+          {initRecording && <div className="recording-indicator"></div>}
+          <span>{formatMinutes(recordingMinutes)}</span>
+          <span>:</span>
+          <span>{formatSeconds(recordingSeconds)}</span>
+        </div>
+        {initRecording && (
+          <div className="cancel-button-container">
+            <button className="cancel-button" title="Cancel recording" onClick={cancelRecording}>
+              <FontAwesomeIcon icon={faTimes} />
+            </button>
+          </div>
+        )}
+      </div>
+      <div className="start-button-container">
+        {initRecording ? (
+          <button
+            className="start-button"
+            title="Save recording"
+            disabled={recordingSeconds === 0}
+            onClick={saveRecording}
+          >
+            <FontAwesomeIcon icon={faSave} size="2x" />
+          </button>
+        ) : (
+          <button className="start-button" title="Start recording" onClick={startRecording}>
+            <FontAwesomeIcon icon={faMicrophone} size="2x" />
+          </button>
+        )}
+      </div>
+    </div>
+  );
+}
